bb51f3f0e32d471c59e1400580e559343feca34f
diribar <diegoiribarren2015@gmail.com>
2024-11-22 20:08:40 -0300

    Avance del proyecto

--- javascript/listadoPersonas.js
+++ javascript/listadoPersonas.js
@@ -2,30 +2,40 @@
 
 window.addEventListener("load", () => {
 	let DOM = {
-		listado: document.querySelector("table .listado"),
+		listado: document.querySelectorAll("table .listado"),
 		agregar: document.querySelector("table .agregar"),
 	};
+	console.log(DOM.listado);
+
 	DOM = {
 		...DOM,
 		// Listado - campos
-		ids: listado.querySelectorAll(".id"),
-		nombres: listado.querySelectorAll("input[name='nombre']"),
-		apellidos: listado.querySelectorAll("input[name='apellido']"),
-		fechasCumple: listado.querySelectorAll("input[name='fechaCumple']"),
-		tieneSeguros: listado.querySelectorAll("select[name='tieneSeguros']"),
+		ids: document.querySelectorAll(".listado .id"),
+		nombres: document.querySelectorAll(".listado input[name='nombre']"),
+		apellidos: document.querySelectorAll(".listado input[name='apellido']"),
+		fechasCumple: document.querySelectorAll(".listado input[name='fechaCumple']"),
+		tieneSeguros: document.querySelectorAll(".listado select[name='tieneSeguros']"),
 
 		// Listado - íconos
-		ediciones: listado.querySelectorAll(".edicion"),
-		eliminars: listado.querySelectorAll(".eliminar"),
-		listadoPelis: listado.querySelectorAll(".listadoPeliculas"),
+		ediciones: document.querySelectorAll(".listado .edicion"),
+		eliminars: document.querySelectorAll(".listado .eliminar"),
+		listadoPelis: document.querySelectorAll(".listado .listadoPeliculas"),
 
 		// Agregar - campos
-		nombre: agregar.querySelector("input[name='nombre']"),
-		apellido: agregar.querySelector("input[name='apellido']"),
-		fechaCumple: agregar.querySelector("input[name='fechaCumple']"),
-		tieneSeguro: agregar.querySelector("select[name='tieneSeguros']"),
+		nombre: DOM.agregar.querySelector("input[name='nombre']"),
+		apellido: DOM.agregar.querySelector("input[name='apellido']"),
+		fechaCumple: DOM.agregar.querySelector("input[name='fechaCumple']"),
+		tieneSeguro: DOM.agregar.querySelector("select[name='tieneSeguros']"),
 
 		// Agregar - íconos
-		confirmar: agregar.querySelector(".confirmar"),
+		confirmar: DOM.agregar.querySelector(".confirmar"),
 	};
+
+	// Eventos
+	DOM.ediciones.forEach((edicion,i)=>{
+		edicion.addEventListener("click",()=>{
+			console.dir(DOM.listado[i])
+			// for (let child of DOM.listado[i])
+		})
+	})
 });
