b41d70714a5b9b6c2cc896c0a8d2d33f47fc7170
diribar <diegoiribarren2015@gmail.com>
2024-11-22 19:59:37 -0300

    Avance del proyecto

--- javascript/listadoPersonas.js
+++ javascript/listadoPersonas.js
@@ -1,5 +1,31 @@
 "use strict";
 
 window.addEventListener("load", () => {
+	let DOM = {
+		listado: document.querySelector("table .listado"),
+		agregar: document.querySelector("table .agregar"),
+	};
+	DOM = {
+		...DOM,
+		// Listado - campos
+		ids: listado.querySelectorAll(".id"),
+		nombres: listado.querySelectorAll("input[name='nombre']"),
+		apellidos: listado.querySelectorAll("input[name='apellido']"),
+		fechasCumple: listado.querySelectorAll("input[name='fechaCumple']"),
+		tieneSeguros: listado.querySelectorAll("select[name='tieneSeguros']"),
 
+		// Listado - íconos
+		ediciones: listado.querySelectorAll(".edicion"),
+		eliminars: listado.querySelectorAll(".eliminar"),
+		listadoPelis: listado.querySelectorAll(".listadoPeliculas"),
+
+		// Agregar - campos
+		nombre: agregar.querySelector("input[name='nombre']"),
+		apellido: agregar.querySelector("input[name='apellido']"),
+		fechaCumple: agregar.querySelector("input[name='fechaCumple']"),
+		tieneSeguro: agregar.querySelector("select[name='tieneSeguros']"),
+
+		// Agregar - íconos
+		confirmar: agregar.querySelector(".confirmar"),
+	};
 });
