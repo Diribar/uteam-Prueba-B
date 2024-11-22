4f19690c073302e2a00dcc595d80c9ec09e4925f
diribar <diegoiribarren2015@gmail.com>
2024-11-22 18:26:28 -0300

    Avance del proyecto

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ global.express = require("express");
 const app = express();
 app.listen(80, () => console.log("Servidor funcionando..."));
 
+global.fs = require("fs");
+
 app.set("view engine", "ejs"); // Terminación de los archivos de vista
 app.set("views", ["./vistas"]);
 
