ba63a384cc41dd1e187b2a83d4f5d37eb4a52204
diribar <diegoiribarren2015@gmail.com>
2024-11-22 18:47:26 -0300

    Avance del proyecto

--- rutasContrs/contrVista.js
+++ rutasContrs/contrVista.js
@@ -3,9 +3,16 @@
 module.exports = {
 	listadoDePersonas: (req, res) => {
 		// Obtiene información del archivo 'json'
-		const rutaNombre = "../datos.json";
+		const rutaNombre = path.join(__dirname, "../datos.json");
 		const json = fs.readFileSync(rutaNombre, "utf8");
-		const info =  JSON.parse(json);
+		const info = JSON.parse(json);
+		const personas = info.map((n) => ({
+			nombre: n["first-name"],
+			apellido: n["last-name"],
+			fechaCumple: n.birthdate,
+			tieneSeguro: n["has-insurance"],
+		}));
+		return res.render("listadoPersonas", {personas});
 	},
 	mostrarPelisDePersona: (req, res) => {},
 };
