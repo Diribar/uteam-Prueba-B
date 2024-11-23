822a91151a3cae32c61fae53938102278c0dda9d
diribar <diegoiribarren2015@gmail.com>
2024-11-23 07:36:41 -0300

    Avance del proyecto

--- rutasContrs/contrAPI.js
+++ rutasContrs/contrAPI.js
@@ -1,18 +1,39 @@
 "use strict";
+const funciones = require("../funciones.js");
 
 module.exports = {
 	// Listado de personas
 	editaPersona: (req, res) => {
-		console.log(req.query);
-		return res.json()
+		// Obtiene los datos
+		const datos = req.query;
+		const datosGuardar = {
+			id: Number(datos.id),
+			"first-name": datos.nombre,
+			"last-name": datos.apellido,
+			birthdate: datos.fechaCumple,
+			"has-insurance": datos.tieneSeguro == "1",
+		};
+
+		// Obtiene el archivo de personas
+		const info = funciones.leerJson("personas");
+
+		// Busca el índice
+		const indice = info.findIndex((n) => n.id == datos.id);
+
+		// Reemplaza el elemento y lo guarda
+		info[indice] = datosGuardar;
+
+
+		// Fin
+		return res.json();
 	},
 	eliminaPersona: (req, res) => {
 		console.log(req.query);
-		return res.json()
+		return res.json();
 	},
 	agregaPersona: (req, res) => {
 		console.log(req.query);
-		return res.json()
+		return res.json();
 	},
 
 	// Películas por persona
